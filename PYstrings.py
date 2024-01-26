0cf17ab5f36bf9283fd471e1c776e038b8c228be
VsdCh <91698305+VsdCh@users.noreply.github.com>
2024-01-26 19:08:06 +0300

    Update PYstrings.py

--- PYstrings.py
+++ PYstrings.py
@@ -3,6 +3,6 @@ line2 = "reSeArcHer"
 print(line1.casefold())
 print(line2.casefold())
 #делает так чтобы все line были строчными буквами
-
 print(line1.capitalize())
 print(line2.capitalize())
+#делает так чтобы все line были с большой буквы
